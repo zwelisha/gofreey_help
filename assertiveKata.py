788cab7f6eec01c59ec577e9a48c1ff80c7afdf4
zwelisha <zwescientist@gmail.com>
2020-12-22 18:44:20 +0200

    Added the first demsonstration of assertive kata, checking the equality of the strings s1 and s2

---
+++ assertiveKata.py
@@ -0,0 +1,13 @@
+user_input = input("Enter your string input")
+
+def assertive_kata(s1,s2):
+    '''
+    s1 and s2 here are two input strings passed by the user.
+    '''
+    # For the first assertion, let's check if the number of chracters is the same.
+    #That is the strings, s1 and s2 have the same length.
+
+    assert len(s1) == len(s2)
+    
+    
+
